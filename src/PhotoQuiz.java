2acb0671c9cf3ee1adade3053274da5c7b6b71a9
GeneralOy <Guest@wts9.attlocal.net>
2015-08-06 16:56:12 -0700

    h
    h

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -10,6 +10,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
+@SuppressWarnings("unused")
 public class PhotoQuiz {
 
 	public static void main(String[] args) throws Exception {
